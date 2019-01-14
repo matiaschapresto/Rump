b132e811a97e4e387c3044019163e73e58a52afb
mchapresto <matias.chapresto@rappi.com>
2019-01-14 14:23:33 -0300

    rocket flies

---
+++ Assets/Follower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public Transform nave;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position = new Vector3(nave.transform.position.x,nave.transform.position.y, -40f);
+    }
+}
---
+++ Assets/Scripts/RocketShipMovementScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShipMovementScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 200;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.freezeRotation = true; // take manual control of rotation
+            transform.Rotate(Vector3.left * speed * Time.deltaTime);
+            rb.freezeRotation = false; // resume physics control of rotation
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.freezeRotation = true; // take manual control of rotation
+            transform.Rotate(Vector3.right *speed * Time.deltaTime);
+            rb.freezeRotation = false; // resume physics control of rotation
+        }
+
+
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag != "LaunchPad" ){
+            Destroy(gameObject);
+        }
+    }
+}
